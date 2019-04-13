6cd80d6c6af28b587bfe68ccc5ae815bb70545f6
AlexeySub <mr.317676@gmail.com>
2019-04-13 23:49:59 +0300

    2

--- byLex.js
+++ byLex.js
@@ -3,15 +3,15 @@
             navigator.geolocation.getCurrentPosition(function(position) {
 
                 var latitude = position.coords.latitude;
-                alert(latitude);
-
+                console.log(latitude);
                 var longitude = position.coords.longitude;
-                alert(longitude)})
+                console.log(longitude);
+
 
           console.log("poslal");
           axios.post('https://susanin-navigate.herokuapp.com/checkbus/', {
-            latitude: this.latitude,
-            longitude: this.longitude})
+            latitude: latitude,
+            longitude: longitude})
             .then(response => {
               const busStop = response.data.busStop;
               alert("Остановка"+" "+busStop+" "+this.latitude+" "+this.longitude);
@@ -21,5 +21,5 @@
               console.log(error);
               console.log("oshibka")
             });
-          setTimeout(run, 10000);
+          setTimeout(run, 10000); })
         }, 10000);
